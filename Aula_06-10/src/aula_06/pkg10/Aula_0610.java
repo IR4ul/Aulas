924da78f91c582e387d5933df33661bcbb0c6778
R4ulM <101829089+IR4ul@users.noreply.github.com>
2022-10-06 21:40:05 -0300

    Add files via upload

---
+++ Aula_06-10/src/aula_06/pkg10/Aula_0610.java
@@ -0,0 +1,89 @@
+package aula_06.pkg10;
+import java.util.Scanner;
+public class Aula_0610 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in); 
+        
+        
+        //Trabalhando com vetor e fazendo comparação de maior e menor 
+        
+        
+        //Ex2
+        
+        /*int numeros[]= new int [15];
+        
+        
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Digiete o" +(i+1)+"o. numero:" );
+            numeros[i]=entrada.nextInt();
+        }
+        int maior = numeros[0];
+        int menor = numeros[0];
+        for (int i = 0; i < 15; i++) {
+            if(numeros[i]>maior){
+                maior = numeros[i];
+            
+            }
+            
+            if(numeros[i]<menor){
+                menor = numeros[i];
+            }
+        }
+        System.out.println("O maior numero e: "+maior);
+        System.out.println("O maior numero e: "+menor );
+        */
+        
+        
+        //ex3
+        
+        /*String nomes[]=new String [3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Digite o "+(i+1)+"o. Nome");
+            nomes[i]= entrada.next();
+        }
+        
+        System.out.println("Ordem Inversa");
+        for (int i = 2; i >= 0 ; i--) {
+            System.out.println(nomes[i]+"\n");
+            
+        }*/
+        
+        
+        //EX3
+        
+        /*
+        String nomes[][] = new  String [3][1];
+        double notas[][]= new double [3][3];
+        double media[]= new double [3];
+
+        
+        for (int linha= 0; linha < 3; linha++) {
+            for (int coluna = 0; coluna < 2; coluna++) {
+                if(coluna==0){
+                System.out.println("Digiteo nome do Aluno ");
+                nomes[linha][coluna] = entrada.next();
+                    System.out.println("Digite a nota N1: ");
+                    notas[linha][coluna]=entrada.nextDouble();
+                }else{
+                    System.out.println("Digite a nota N2");
+                    notas[linha][coluna]=entrada.nextDouble();
+                }  
+            }
+            media[linha]=notas[linha][0]+0.4+notas[linha][1]*0.6;
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println("A media do(a) Aluno"+nomes[i][0]+" e:"+media[i]);
+        }*/
+        
+        
+        //EX4
+        
+        //Não consegui acompanhar , pedir codigo pro vitor :)
+        
+        
+        //EX5 
+        
+    } 
+
+    
+
